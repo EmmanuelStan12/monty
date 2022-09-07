c87dce6909c1eff7b481b535f053e95c2b2f0b4f
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-09-07 10:41:44 +0100

    task: Swap
    Swaps the first and second element of a stack

--- main.c
+++ main.c
@@ -4,7 +4,8 @@ instruction_t instructions[] = {
 	{"push", push},
 	{"pall", pall},
 	{"pint", pint},
-	{"pop", pop}
+	{"pop", pop},
+	{"swap", swap}
 };
 
 int stack_size = 0;
---
+++ swap.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * swap - swaps the top two elements of a stack
+ * @stack: the stack
+ * @line_number: the line number
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first, *second, *next;
+	if (stack_size < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short", line_number);
+		exit(EXIT_FAILURE);
+	}
+	first = *stack;
+	second = first->next;
+	next = second->next;
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+	first->next = next;
+	if (next)
+		next->prev = first;
+	*stack = second;
+}
